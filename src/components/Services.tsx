691d45377fa46638799559d119c696a240ac8aa4
jihansoft <jihanuddinsoft@gmail.com>
2025-05-02 18:57:01 +0600

    error fix

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 
-const serviceData = [
-  {
-    id: 1,
-    name: "Social Media Marketing",
-    img: "/images/",
-    dec: "Our UX design services are focused on creating intuitive, enjoyable, and efficient digital experiences.",
-  },
-];
+// const serviceData = [
+//   {
+//     id: 1,
+//     name: "Social Media Marketing",
+//     icon: "/images/service1.png",
+//     dec: "Our UX design services are focused on creating intuitive, enjoyable, and efficient digital experiences.",
+//   },
+//   {
+//     id: 2,
+//     name: "Business Consulting",
+//     icon: "/images/service2.png",
+//     dec: "Our UX design services are focused on creating intuitive, enjoyable, and efficient digital experiences.",
+//   },
+//   {
+//     id: 3,
+//     name: "Lead generation",
+//     icon: "/images/service3.png",
+//     dec: "Our UX design services are focused on creating intuitive, enjoyable, and efficient digital experiences.",
+//   },
+//   {
+//     id: 4,
+//     name: "Web/App Development",
+//     icon: "/images/service4.png",
+//     dec: "Our UX design services are focused on creating intuitive, enjoyable, and efficient digital experiences.",
+//   },
+// ];
 export default function Services() {
   return <div className="bg-white"></div>;
 }
